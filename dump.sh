ce9e86ef8c4ac3f810f3388fb450f7e812ae8277
Anton Iškov <iskov.anton@yandex.com>
2015-10-14 13:49:37 +0200

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -8,4 +8,4 @@ cd ${0%/*}
 [ -f ./log/error.log ] && rm ./log/error.log
 [ -f ./log/main.log ] && rm ./log/main.log
 
-python dump.py 2>> ./log/error.log >> ./log/main.log
+python dump.py "$1" 2>> ./log/error.log >> ./log/main.log
