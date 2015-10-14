434baf5d4575098dedfab021b98cc77f0c29df6f
Anton Iškov <iskov.anton@yandex.com>
2015-10-14 13:50:12 +0200

    Update dump.py

--- dump.py
+++ dump.py
@@ -1,10 +1,15 @@
+import sys
 import json
 import os
 import datetime
 
 
-def mongodb(rs, hosts, out): 
+def mongodb(rs, hosts, out):
     os.system('mongodump --host %s/%s --out="%s"' % (rs, ','.join(hosts), out))
+
+
+def mariadb(host, out):
+    os.system('mysqldump --host=%s --single-transaction --skip-comments --all-databases > %s' % (host, out))
 
 
 def archivate(target, out):
@@ -24,23 +29,44 @@ def ln(target, out):
     os.system('ln -s %s --target-directory="%s"' % (target, out))
 
 
+def echo(message):
+    os.system('echo "%s"' % message)
+
+
 # Execution
+echo('Dump execution:')
+
 root_dir = os.path.dirname(os.path.abspath(__file__))
 with open(root_dir + '/config.json') as f:
     config = json.load(f)
 
-if 'mongodb' in config:
+if len(sys.argv) < 2 or not sys.argv[1]:
+    echo('Bad request: %s.' % sys.argv)
+    exit(1)
+
+task = sys.argv[1]
+echo('Task selected: %s.' % task)
+
+if task == 'mongodb' and 'mongodb' in config:
     mongodbConfig = config['mongodb']
     mongodb(mongodbConfig['rs'], mongodbConfig['hosts'], './tmp')
     name = '%s_mongodb' % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     name = archivate('./tmp/*', name)
+
+if task == 'mariadb' and 'mariadb' in config:
+    mariadbConfig = config['mariadb']
+    mariadb(mariadbConfig['host'], './tmp/dump.sh')
+    name = '%s_mariadb' % datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    name = archivate('./tmp/*', name)
+
+if 'name' in locals():
     rm('./tmp')
-    mv(name, mongodbConfig['out'])
-    
+    mv(name, config['out'])
+    name = '%s/%s' % (config['out'], name)
+    echo('Dump created: %s.' % name)
+
     if 'distribution' in config:
-        ln('%s/%s' % (mongodbConfig['out'], name), config['distribution'])
-
-
-if 'mysql' in config:
-    # TODO: implement dump
-    pass
+        ln(name, config['distribution'])
+else:
+    echo('Wrong configuration')
+    exit(1)
